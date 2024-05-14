ab92c019e5ddb11086eaecde99b6d021cfe5ed31
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-05-14 04:04:30 +0400

    cover

---
+++ script.js
@@ -0,0 +1,15 @@
+let burgerBtn = document.querySelector(".burger");
+let xMark = document.querySelector(".xMark");
+let mobileMenu = document.querySelector(".mobile_menu");
+
+burgerBtn.addEventListener("click", () => {
+  xMark.style.display = "block";
+  burgerBtn.style.display = "none";
+  mobileMenu.style.left = "0";
+});
+
+xMark.addEventListener("click", () => {
+  burgerBtn.style.display = "block";
+  xMark.style.display = "none";
+  mobileMenu.style.left = "-100%";
+});
