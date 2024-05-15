a12d712489a9203b4d618205395f0aae603837f4
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-05-15 21:35:49 +0400

    last section on home page

--- script.js
+++ script.js
@@ -15,19 +15,3 @@ xMark.addEventListener("click", () => {
   xMark.style.display = "none";
   mobileMenu.style.left = "-100%";
 });
-
-// cards hover
-
-let cardHover = document.querySelector(".cards2_hover");
-let figure = document.querySelector(".individualcards2");
-let remowe = document.querySelector(".cards2_text");
-
-figure.addEventListener("mouseenter", () => {
-  remowe.style.display = "none";
-  cardHover.style.display = "block";
-});
-
-figure.addEventListener("mouseleave", () => {
-  remowe.style.display = "block";
-  cardHover.style.display = "none";
-});
