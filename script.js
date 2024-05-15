2435197fe88bb1336963a9c2b1591054b16c5050
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-05-15 04:54:40 +0400

    section 5 other cards on index

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// burger
+
 let burgerBtn = document.querySelector(".burger");
 let xMark = document.querySelector(".xMark");
 let mobileMenu = document.querySelector(".mobile_menu");
@@ -12,4 +14,20 @@ xMark.addEventListener("click", () => {
   burgerBtn.style.display = "block";
   xMark.style.display = "none";
   mobileMenu.style.left = "-100%";
+});
+
+// cards hover
+
+let cardHover = document.querySelector(".cards2_hover");
+let figure = document.querySelector(".individualcards2");
+let remowe = document.querySelector(".cards2_text");
+
+figure.addEventListener("mouseenter", () => {
+  remowe.style.display = "none";
+  cardHover.style.display = "block";
+});
+
+figure.addEventListener("mouseleave", () => {
+  remowe.style.display = "block";
+  cardHover.style.display = "none";
 });
